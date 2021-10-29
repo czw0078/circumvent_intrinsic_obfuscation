b87d4826b3ad4dd1326cf84f8add22528543ca77
CZW0078 <czw0078@auburn.edu>
2021-10-29 22:18:08 +0000

    add autoattack dev version

--- utility.py
+++ utility.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 
 class LeNet(nn.Module):
-    def __init__(self):
+    def __init__(self): # TODO change the name from conv1 to cnn1
         super(LeNet, self).__init__() # can be just super() in python 3
         self.conv1 = nn.Sequential(         
             nn.Conv2d(
@@ -31,6 +31,42 @@ class LeNet(nn.Module):
         output = self.out(x)
         return output    # do not return x for visualization
 
+def network():
+    net = torch.load('./data/LeNet.pt')
+    net.eval()
+    return net
+
+def samples(x=None,adversarial=True):
+    lbl100=torch.load('./data/samples/lbl100.pt')
+    if x != None:
+        return [(x, lbl100)] 
+    elif not adversarial:
+        img100=torch.load('./data/samples/img100.pt')
+        return [(img100, lbl100)]
+    else:
+        adv100=torch.load('./data/samples/adv100.pt')
+        return [(adv100, lbl100)]
+
+def identity(x, edge):
+    return x
+
+def evaluate(net, loader, defense=identity, edge=0.3, device=torch.device("cpu")):
+    with torch.no_grad(): 
+        correct = 0
+        total = 0
+        for images, labels in loader:
+            images, labels = images.to(device), labels.to(device)
+            
+            test_output = net(defense(images, edge)) # output are in loader batch size
+            # max work on second dimention of batch 
+            # variable to tensorsqueeze for python 2
+            pred_y = torch.max(test_output, 1)[1].data.squeeze()
+            # print(pred_y) print(labels)
+            correct += (pred_y == labels).sum().item()
+            total += labels.size(0)
+        accuracy = correct / float(total)
+        print('Check accuracy of the model:', accuracy)
+
 def trimmer(x, edge=0.3): # 0 to 0.5
     """
     >>> import torch
@@ -42,14 +78,15 @@ def trimmer(x, edge=0.3): # 0 to 0.5
 
     low = edge
     high = 1 - edge
+    y = torch.clone(x)
 
-    select_high = x >= high
-    select_low = x <= low
+    select_high = y >= high
+    select_low = y <= low
 
-    x[select_high] = 1
-    x[select_low] = 0
+    y[select_high] = 1
+    y[select_low] = 0
 
-    return x
+    return y
 
 if __name__=="__main__":
     import doctest
